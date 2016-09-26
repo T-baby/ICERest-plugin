516d0cc5f99129580cd8acaabb4588a95327c732
t-baby <t-baby@besdlab.cn>
2016-09-26 10:39:35 +0800

    去除中文注释

--- src/main/java/org/beetl/core/Template.java
+++ src/main/java/org/beetl/core/Template.java
@@ -16,11 +16,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-/**
- * 模板类
- *
- * @author joelli
- */
+
 public class Template {
     public Program program;
     public Configuration cf;
@@ -35,36 +31,19 @@ public class Template {
         this.gt = gt;
     }
 
-    /**
-     * 获取模板输出的文本，结果是一个String
-     *
-     * @return
-     * @throws BeetlException
-     */
+
     public String render() throws BeetlException {
         StringWriter sw = new StringWriter();
         renderTo(sw);
         return sw.toString();
     }
 
-    /**
-     * 获取模板输出的文本,输出到Writer里
-     *
-     * @param writer
-     * @throws BeetlException
-     */
     public void renderTo(Writer writer) throws BeetlException {
         ByteWriter_Char byteWriter = new ByteWriter_Char(writer, cf.charset, ctx);
         this.renderTo(byteWriter);
 
     }
 
-    /**
-     * 获取模板输出的文本,输出到OutputStream里,tempalte必须在二进制输出模式下才能使用
-     *
-     * @param os
-     * @throws BeetlException
-     */
     public void renderTo(OutputStream os) throws BeetlException {
         ByteWriter_Byte byteWriter = new ByteWriter_Byte(os, cf.charset, ctx);
         this.renderTo(byteWriter);
@@ -102,7 +81,7 @@ public class Template {
                 e.pushResource(this.program.res.id);
             }
 
-            // 是否打印异常，只有根模板才能打印异常
+            // 锟角凤拷锟接★拷斐ｏ拷锟街伙拷懈锟侥ｏ拷锟斤拷锟杰达拷印锟届常
             if (!isRoot)
                 throw e;
 
@@ -142,14 +121,7 @@ public class Template {
 
     }
 
-    /**
-     * 为模板绑定变量，此变量在模板编译的时候,根据infer标记来决定是否要推测期类型，如果dynamic为true，
-     * 则表示模板引擎优化不需要推测其类型，默认总是false，即变量总是对应同一个类型。如果为true，则认为: 因为变量可能能对应不同java类型，
-     * 或者变量是容器，但容器里的元素是不同类型
-     *
-     * @param varName
-     * @param o
-     */
+
     public void binding(String varName, Object o, boolean dynamic) {
         ctx.set(varName, o, dynamic);
         // ctx.globalVar.put(varName, o);
@@ -158,12 +130,7 @@ public class Template {
         }
     }
 
-    /**
-     * 为模板绑定一个变量，infer是true
-     *
-     * @param varName 变量名称，必须符合jaavascript 命名规范
-     * @param o       模板变量
-     */
+
     public void binding(String varName, Object o) {
         this.binding(varName, o, false);
     }
@@ -176,11 +143,7 @@ public class Template {
         this.ctx.objectKeys.add(key);
     }
 
-    /**
-     * 为模板绑定多个变量，map的key，value对应了变量名称和变量值。key必须符合javascript命名规范
-     *
-     * @param map
-     */
+
     public void binding(Map map) {
         Map<String, Object> values = map;
         if(values==null) return ;
@@ -204,7 +167,7 @@ public class Template {
     //		if (ctx.isInit)
     //		{
     //			ctx.globalVar = map;
-    //			// 重用
+    //			// 锟斤拷锟斤拷
     //			for (int i = ctx.tempVarStartIndex; i < ctx.vars.length; i++)
     //			{
     //				ctx.vars[i] = null;
